df925d471e68f1bc47307dfad63e7e8e81ccb9ee
Valentin B <valentin.be@protonmail.com>
2019-10-16 21:55:01 +0200

    Move event handler to event_handler.rs and add diesel for Postgres integration

---
+++ src/event_handler.rs
@@ -0,0 +1,19 @@
+use log::info;
+use serenity::model::event::ResumedEvent;
+use serenity::{model::gateway::Ready, prelude::*};
+
+pub struct DiscordHandler;
+
+impl EventHandler for DiscordHandler {
+    fn ready(&self, _: Context, ready: Ready) {
+        info!(
+            "{} with ID {} is connected!",
+            ready.user.tag(),
+            ready.user.id
+        );
+    }
+
+    fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Connection resumed!");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,46 @@
+extern crate chrono;
+
+#[macro_use]
+extern crate diesel;
+
+extern crate log;
+
+#[macro_use]
+extern crate serenity;
+
 use std::env;
 
-use serenity::{
-    model::{channel::Message, gateway::Ready},
-    prelude::*,
-};
+use serenity::prelude::Client;
 
-struct Handler;
+use self::event_handler::DiscordHandler;
+use diesel::{Connection, PgConnection};
 
-impl EventHandler for Handler {
-    fn ready(&self, _: Context, ready: Ready) {
-        println!("Bot {} with ID {} is connected!", ready.user.tag(), ready.user.id);
-    }
+mod event_handler;
+
+#[inline]
+fn create_database_url() -> String {
+    let user = env::var("POSTGRES_USER")
+        .expect("Please set the POSTGRES_USER environment variable before running the bot.");
+    let password = env::var("POSTGRES_PASSWORD")
+        .expect("Please set the POSTGRES_PASSWORD environment variable before running the bot.");
+    let host = env::var("POSTGRES_HOST")
+        .expect("Please set the POSTGRES_HOST environment variable before running the bot.");
+    let database = env::var("POSTGRES_DATABASE")
+        .expect("Please set the POSTGRES_DATABASE environment variable before running the bot.");
+
+    format!("postgresql://{}:{}@{}/{}", user, password, host, database)
 }
 
 fn main() {
     let token = env::var("DISCORD_TOKEN")
         .expect("Please set the DISCORD_TOKEN environment variable before running the bot.");
 
-    let mut client = Client::new(&token, Handler)
-        .expect("An error occurred during client creation.");
+    // Currently unused.
+    let postgres_connection = PgConnection::establish(&create_database_url())
+        .expect("Failed to establish a connection to the database!");
+
+    let mut client =
+        Client::new(&token, DiscordHandler).expect("An error occurred during client creation.");
 
     if let Err(why) = client.start() {
         println!("Failed to connect: {:?}", why);
