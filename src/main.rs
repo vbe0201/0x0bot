6ddeaebf707c1e8120ea103e622f50bde2621736
Valentin B <valentin.be@protonmail.com>
2019-10-16 17:41:23 +0200

    Add initial bot structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("Bot {} with ID {} is connected!", ready.user.tag(), ready.user.id);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Please set the DISCORD_TOKEN environment variable before running the bot.");
+
+    let mut client = Client::new(&token, Handler)
+        .expect("An error occurred during client creation.");
+
+    if let Err(why) = client.start() {
+        println!("Failed to connect: {:?}", why);
+    }
 }
